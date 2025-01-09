66ffc1bf29d6d9bb56af9ad8f3612cfb56b9e24b
elitheski <ebfagbote@email.neit.edu>
2025-01-09 17:10:27 -0500

    W1D2 finished demo
    Just review of last term

---
+++ pratice/week1/lab1.py
@@ -0,0 +1,15 @@
+#Elijah Fabote
+#W1D2 Lab 1
+#1-9-2025 [W1D2]
+
+#PROGRAM PROMT
+'''you will be writing one Python file for this project - it is a program that determines whether a
+meeting room is in violation of fire regulations regarding the maximum room capacity. The
+program will accept the maximum room capacity and the number of people attending the
+meeting. If the number of people is less than or equal to the maximum room capacity, the
+program announces that it is legal to hold the meeting and tells how many additional people may
+legally attend. If the number of people exceeds the maximum room capacity, the program
+announces that the meeting cannot be held as planned due to the fire regulation and tells how
+many people must be excluded in order to meet the fire regulations. The user should be allowed
+to enter and check as many rooms as they would like without exiting the program.'''
+
